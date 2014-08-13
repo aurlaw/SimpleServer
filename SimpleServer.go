c04e82b7b42e9e04d9174b61326dcbe636215af5
Michael Lawrence <mlawrence@aurlaw.com>
2014-08-12 21:50:12 -0400

    updated

--- SimpleServer.go
+++ SimpleServer.go
@@ -1,39 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "strings"
-    "flag"
-    "strconv"
-)   
+	"flag"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
 
 var chttp = http.NewServeMux()
 
 func main() {
 
-    homeDir := flag.String("dir", "./", "directory of files to serve")
-    port := flag.Int("port", 8080, "port to listen on")
+	homeDir := flag.String("dir", "./", "directory of files to serve")
+	port := flag.Int("port", 8080, "port to listen on")
 
-    // var homeDir string
-    // flag.StringVar(&homeDir, "dir", "./", "directory of files to server")
+	flag.Parse()
 
-    flag.Parse();
+	fmt.Println("dir:", *homeDir)
+	fmt.Println("port:", *port)
 
-    fmt.Println("dir:", *homeDir)
-    fmt.Println("port:", *port)
+	chttp.Handle("/", http.FileServer(http.Dir(*homeDir)))
 
-    chttp.Handle("/", http.FileServer(http.Dir(*homeDir)))
-
-    http.HandleFunc("/", HomeHandler) // homepage
-    http.ListenAndServe(":" + strconv.Itoa(*port), nil)
-}   
+	http.HandleFunc("/", HomeHandler) // homepage
+	http.ListenAndServe(":"+strconv.Itoa(*port), nil)
+}
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
-    if (strings.Contains(r.URL.Path, ".")) {
-        chttp.ServeHTTP(w, r)
-    } else {
-        fmt.Fprintf(w, "HomeHandler")
-    }   
-} 
+	if strings.Contains(r.URL.Path, ".") {
+		chttp.ServeHTTP(w, r)
+	} else {
+		fmt.Fprintf(w, "HomeHandler")
+	}
+}
